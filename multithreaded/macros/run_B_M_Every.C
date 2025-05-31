2653533cf7e8cd6f35008f9395b2877064be1d0d
Adam Szabelski <aszabelski@wn42083.cis.gov.pl>
2025-05-31 09:38:03 +0200

    Adding a free parameter to the shapes. In sceario A - frac, in scenario B - skew is released

--- multithreaded/macros/run_B_M_Every.C
+++ multithreaded/macros/run_B_M_Every.C
@@ -1,6 +1,7 @@
 void run_B_M_Every(string config_file){
 	TStopwatch t;
 	t.Start();
+	gSystem->AddIncludePath("-I/mnt/opt/spack/0.20/opt/spack/linux-centos7-ivybridge/gcc-12.3.0/boost-1.82.0-bntx2bbdfbbpoq52tjj5zvl5ddbngidf/include");
 	gInterpreter->AddIncludePath("../include");
 	gInterpreter->AddIncludePath("../../common/include");
 	gROOT->ProcessLine(".L ../../common/src/M_B_2missPT_fit.cpp+");
